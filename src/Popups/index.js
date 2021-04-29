7c37bdfc8f9eb4096dcef771be883801e4005114
geospatial-dan <daniel.kinder@stockport.gov.uk>
2021-04-29 15:04:44 +0100

    Margin amended for report it button

--- src/Popups/index.js
+++ src/Popups/index.js
@@ -18,16 +18,17 @@ const gulliesFaultPopup = feature => {
   const varName = getTargetUrl()
 
   return `<div>
-  <div class="item"><p class="title">Location </p><p class="info">${feature.properties.street}</p></div>
-    <div class="govuk-panel smbc-panel--error govuk-!-padding-1">
-      <div class="smbc-panel__body smbc-!-font-color-white">
-        A flood has already been reported here
-      </div>
+  <p class="smbc-body govuk-!-font-size-14 govuk-!-margin-0 smbc-!-font-color-white">Location</p>
+  <p class="smbc-body govuk-!-margin-bottom-4 govuk-!-margin-top-0 smbc-!-font-color-white">${feature.properties.street}</p>
+  <div class="govuk-panel smbc-panel--error govuk-!-padding-1">
+    <div class="smbc-panel__body smbc-!-font-color-white">
+        A blocked drain has already been reported
     </div>
-    <hr class="govuk-section-break govuk-section-break--m govuk-section-break--visible">
-    <a class="govuk-button govuk-!-margin-bottom-0 govuk-!-margin-top-4" href="${varName}/track-a-report/details/${feature.properties.ext_system_ref}">View this report</a>
-    <a class="govuk-button govuk-!-margin-bottom-0 govuk-!-margin-top-4" href="https://www.stockport.gov.uk/">Go to the homepage</a>
-    </div>`
+  </div>
+  <hr class="govuk-section-break govuk-section-break--m govuk-section-break--visible">
+  <a class="govuk-button govuk-button--secondary govuk-!-margin-bottom-0" href="${varName}/track-a-report/details/${feature.properties.ext_system_ref}">View this report</a>
+  <a class="govuk-button govuk-!-margin-bottom-0 govuk-!-margin-top-4" href="https://www.stockport.gov.uk/">Go to the homepage</a>
+</div>`
 }
 
 const gulliesPopup = (feature, layer) => {
