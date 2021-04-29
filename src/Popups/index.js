dafc6e87a530ece2f40821dd1c82ee615b1e9a75
geospatial-dan <daniel.kinder@stockport.gov.uk>
2021-04-29 15:22:09 +0100

    Margins and width styling

--- src/Popups/index.js
+++ src/Popups/index.js
@@ -3,13 +3,14 @@ import { getTargetUrl } from '../Helpers'
 const gulliesActivePopup = feature => {
   //const varName = getTargetUrl()
 
-  return `<div class="item"><p class="title">Location </p><p class="info">${feature.properties.street}</p></div>
+  return `<div> <p class="smbc-body govuk-!-font-size-14 govuk-!-margin-0 smbc-!-font-color-white">Location</p>
+  <p class="smbc-body govuk-!-margin-bottom-4 govuk-!-margin-top-0 smbc-!-font-color-white">${feature.properties.street}</p></div>
   <hr class="govuk-section-break govuk-section-break--m govuk-section-break--visible">
   <input id="siteCode" name="siteCode" type="hidden" value="${feature.properties.site_code}">
   <input id="assetId" name="assetId" type="hidden" value="${feature.properties.central_asset_id}">
   <input id="easting" name="easting" type="hidden" value="${feature.properties.easting}">
   <input id="northing" name="northing" type="hidden" value="${feature.properties.northing}">
-  <button class="govuk-button govuk-!-margin-bottom-0 govuk-!-margin-top-4" data-module="govuk-button">
+  <button class="govuk-button govuk-!-margin-bottom-0 smbc-!-width-100" data-module="govuk-button">
     Report this gully
   </button>`
 }
@@ -26,8 +27,8 @@ const gulliesFaultPopup = feature => {
     </div>
   </div>
   <hr class="govuk-section-break govuk-section-break--m govuk-section-break--visible">
-  <a class="govuk-button govuk-button--secondary govuk-!-margin-bottom-0" href="${varName}/track-a-report/details/${feature.properties.ext_system_ref}">View this report</a>
-  <a class="govuk-button govuk-!-margin-bottom-0 govuk-!-margin-top-4" href="https://www.stockport.gov.uk/">Go to the homepage</a>
+  <a class="govuk-button govuk-button--secondary govuk-!-margin-bottom-0 smbc-!-width-100" href="${varName}/track-a-report/details/${feature.properties.ext_system_ref}">View this report</a>
+  <a class="govuk-button govuk-!-margin-bottom-0 govuk-!-margin-top-4 smbc-!-width-100" href="https://www.stockport.gov.uk/">Go to the homepage</a>
 </div>`
 }
 
